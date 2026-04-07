25fd3f9cf4f9f0133786ffe7c792cc3a04a226ad
jaswan <jaswanlogan03@gmail.com>
2026-04-07 13:54:21 +0530

    Allow Vercel frontend in CORS

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,13 @@ const app = express();
 // Init Middleware
 app.use(express.json({ extended: false }));
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174'],
+  origin: [
+    'http://localhost:5173', 
+    'http://localhost:5174', 
+    'http://127.0.0.1:5173', 
+    'http://127.0.0.1:5174',
+    'https://task-management-zeta-orcin.vercel.app'
+  ],
   credentials: true,
 }));
 
